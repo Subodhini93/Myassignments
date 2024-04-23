956b8cb15f587e09a5abe4f751646ca3aa09e671
Subodhini93 <164895537+Subodhini93@users.noreply.github.com>
2024-04-23 20:12:18 +0100

    Add files via upload

---
+++ ChildClassBMW.java
@@ -0,0 +1,21 @@
+package myassignments3;
+
+public class ChildClassBMW extends ParentClassCar {
+
+	public void sunroof()  {
+		System.out.println("sunroof is coming from child class");
+	}
+	
+	public static void main(String[] args) {
+		
+		ChildClassBMW cc = new ChildClassBMW();
+		cc.applyBreak();
+		cc.horn();
+		cc.gear();
+		cc.sunroof();
+		
+	}
+}
+// here u can see when u extend to parent class u get the properties of parent and grand parent 
+// because when you go to the parent class there the parent class is already extended to grandparent
+// hence we get here four properties
